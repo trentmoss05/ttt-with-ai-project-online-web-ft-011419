f2df6ada4d298c4bb146760c49333871883abe5a
Trent Moss <trentmoss05@gmail.com>
2019-02-08 01:41:12 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -3,8 +3,9 @@ class Game
   attr_accessor :board, :player_1, :player_2
 
   def initialize(board = Board.new, player_1, player_2)
-    @board = Board.new
-    player
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
   end
 
 end
