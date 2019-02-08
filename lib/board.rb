1cbbbecaefb88a21a92c121a76e690617429ffd7
Trent Moss <trentmoss05@gmail.com>
2019-02-08 01:16:03 +0000

    Automatically backed up by Learn

---
+++ lib/board.rb
@@ -0,0 +1,25 @@
+class Board
+  attr_accessor :cells
+
+  def initialize
+  end
+
+  def reset!
+  end
+
+  def display
+  end
+
+  def position
+  end
+
+  def update
+
+  end
+
+  def token
+  end
+
+  def full?
+  end
+end
