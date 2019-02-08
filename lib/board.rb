cf3600f16c80e5fe275fc7f97089edbe9e5db7ba
Trent Moss <trentmoss05@gmail.com>
2019-02-08 01:23:22 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,7 @@ class Board
   attr_accessor :cells
 
   def initialize
-    @cells = Array.new(9, " ")
+    reset!
   end
 
   def reset!
@@ -13,7 +13,8 @@ class Board
     @cells
   end
 
-  def position
+  def position(input)
+    
   end
 
   def update
